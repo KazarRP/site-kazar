8217fc07b16acfaf0ce56406cc87266bc0c24c60
Jago7480 <alexandre.riquez@gmail.com>
2023-03-22 15:33:47 +0100

    ajout d'eewa

--- proto_json.md
+++ proto_json.md
@@ -1,51 +1,74 @@
-Gameplay
+Chaîne de caractères: une chaîne de caractères est utilisée pour stocker des données textuelles, telles que des noms, des adresses et des descriptions. Exemple: "John Doe".
 
-Race :
+Nombre: un nombre est utilisé pour stocker des données numériques, telles que des âges, des montants et des quantités. Exemple: 35.
 
-Statistiques (Force, Agilité, Vitalité, Autorité, Cran, Expertise, Sorcery, Vie max)
-race_stat1
-race_stat_number1 (en chiffre)
-race_stat2
-race_stat_number2 (en chiffre)
-race_stat3
-race_stat_number3 (en chiffre)
-race_stat4
-race_stat_number4 (en chiffre)
-race_stat5
-race_stat_number5 (en chiffre)
-race_stat6
-race_stat_number6 (en chiffre)
+Booléen: un booléen est utilisé pour stocker des données booléennes (vraies ou fausses). Exemple: true.
 
-Aptitudes raciales
-race_feat1
-race_feat2
-race_feat3
-race_feat4
-race_feat5
-race_feat6
+Objet: un objet est utilisé pour stocker un groupe de données connexes dans une structure complexe. Les objets sont définis à l'aide de paires de clés-valeurs. Exemple: {"nom": "John", "age": 35}.
 
-Classe :
+Tableau: un tableau est utilisé pour stocker une collection de données dans une structure séquentielle. Les éléments du tableau peuvent être de n'importe quel type de données. Exemple: ["John", "Doe", 35].
 
-Statistiques (Force, Agilité, Vitalité, Autorité, Cran, Expertise, Sorcery, Vie max)
-primary_stat
-secondary_stat
-minor_stat
+Null: null est utilisé pour représenter une valeur nulle ou inconnue. Exemple: null.
 
-Statistiques autres
-hp_increase (en %)
-armor_per_grit (en chiffre)
-base_crit (en %)
-crit_chance_scale (Force, Agilité, Vitalité, Autorité, Cran, Expertise, Sorcery, Vie max)
-base_crit_damage (en %)
-crit_dmg_scale (Force, Agilité, Vitalité, Autorité, Cran, Expertise, Sorcery, Vie max)
-crit_scale_rate (en %)
+==========
 
-Aptitudes de classe
-class_feat1
-class_feat2
-class_feat3
-class_feat4
-class_feat5
-class_feat6
-class_feat7
-class_feat8
+"gameplay"
+
+    "racial_bonus"
+
+        "racial_attribute_1"
+        "racial_attribute_2"
+        "racial_attribute_3"
+        "racial_attribute_4"
+        "racial_attribute_5"
+        "racial_attribute_6"
+        "racial_attribute_7"
+        "racial_attribute_8"
+
+    "racial_feats"
+
+        "racial_feat_1"
+        "racial_feat_2"
+        "racial_feat_3"
+        "racial_feat_4"
+        "racial_feat_5"
+        "racial_feat_6"
+
+    "class_bonus"
+
+        "primary_attribute"
+        "secondary_attribute"
+        "minor_attribute"
+        "hp_increase"
+        "armor_per_grit"
+        "base_crit"
+        "crit_chance_scale"
+        "crit_scale_rate"
+        "base_crit_damage"
+        "crit_dmg_scale"
+        "crit_scale_rate"
+
+    "class_feats"
+
+        "class_feat_1"
+        "class_feat_2"
+        "class_feat_3"
+        "class_feat_4"
+        "class_feat_5"
+        "class_feat_6"
+        "class_feat_7"
+        "class_feat_8"
+
+    "gameplay_spells"
+
+        "indrid_spells"
+        "indrid_auras"
+        "indrid_shapeshifts"
+        "eewa_spells"
+        "eewa_auras"
+
+"roleplay"
+
+    "racial_bonus"
+
+        "
--- script_liste.js
+++ script_liste.js
@@ -1,4 +1,5 @@
 // Recherche dans les balises summary
+
 function searchSummaries() {
   const searchTerm = document
     .getElementById("searchTerm")
@@ -32,6 +33,7 @@ function searchSummaries() {
 }
 
 // affiche le bouton back to top après scroll
+
 window.onscroll = function () {
   scrollFunction();
 };
@@ -57,67 +59,69 @@ function scrollToTop() {
 }
 
 // Récupération des éléments HTML correspondant aux listes de filtres
-const categoryFilterSubraces = document.getElementById("category-filter-subraces");
-const categoryFilterSubclasses = document.getElementById("category-filter-subclasses");
-const categoryFilterCurses = document.getElementById("category-filter-curses");
+
+// const categoryFilterSubraces = document.getElementById("category-filter-subraces");
+// const categoryFilterSubclasses = document.getElementById("category-filter-subclasses");
+// const categoryFilterCurses = document.getElementById("category-filter-curses");
 
 // Ajout d'un événement "change" à chaque liste de filtres
-categoryFilterSubraces.addEventListener("change", searchByTags);
-categoryFilterSubclasses.addEventListener("change", searchByTags);
-categoryFilterCurses.addEventListener("change", searchByTags);
+
+// categoryFilterSubraces.addEventListener("change", searchByTags);
+// categoryFilterSubclasses.addEventListener("change", searchByTags);
+// categoryFilterCurses.addEventListener("change", searchByTags);
 
 // récupère les tags sélectionnés et recherche les correspondances dans les balises details
 
-function searchByTags() {
-  const selectedSubraces = [];
-  const selectedSubclasses = [];
-  const selectedCurses = [];
-  const categoryFilterSubraces = document.getElementById("category-filter-subraces");
-  const categoryFilterSubclasses = document.getElementById("category-filter-subclasses");
-  const categoryFilterCurses = document.getElementById("category-filter-curses");
-  for (let i = 0; i < categoryFilterSubraces.options.length; i++) {
-    if (categoryFilterSubraces.options[i].selected) {
-      selectedSubraces.push(categoryFilterSubraces.options[i].value);
-    }
-  }
-  for (let i = 0; i < categoryFilterSubclasses.options.length; i++) {
-    if (categoryFilterSubclasses.options[i].selected) {
-      selectedSubclasses.push(categoryFilterSubclasses.options[i].value);
-    }
-  }
-  for (let i = 0; i < categoryFilterCurses.options.length; i++) {
-    if (categoryFilterCurses.options[i].selected) {
-      selectedCurses.push(categoryFilterCurses.options[i].value);
-    }
-  }
+// function searchByTags() {
+//   const selectedSubraces = [];
+//   const selectedSubclasses = [];
+//   const selectedCurses = [];
+//   const categoryFilterSubraces = document.getElementById("category-filter-subraces");
+//   const categoryFilterSubclasses = document.getElementById("category-filter-subclasses");
+//   const categoryFilterCurses = document.getElementById("category-filter-curses");
+//   for (let i = 0; i < categoryFilterSubraces.options.length; i++) {
+//     if (categoryFilterSubraces.options[i].selected) {
+//       selectedSubraces.push(categoryFilterSubraces.options[i].value);
+//     }
+//   }
+//   for (let i = 0; i < categoryFilterSubclasses.options.length; i++) {
+//     if (categoryFilterSubclasses.options[i].selected) {
+//       selectedSubclasses.push(categoryFilterSubclasses.options[i].value);
+//     }
+//   }
+//   for (let i = 0; i < categoryFilterCurses.options.length; i++) {
+//     if (categoryFilterCurses.options[i].selected) {
+//       selectedCurses.push(categoryFilterCurses.options[i].value);
+//     }
+//   }
   
-  const detailsTags = document.getElementsByTagName("details");
-  const results = [];
-  for (let i = 0; i < detailsTags.length; i++) {
-    const tags = detailsTags[i].getAttribute("data-tags").split(",");
-    const text = detailsTags[i].textContent.toLowerCase();
-    const foundSubrace = tags.some(tag => selectedSubraces.includes(tag) && tag !== '');
-    const foundSubclass = tags.some(tag => selectedSubclasses.includes(tag) && tag !== '');
-    const foundCurse = tags.some(tag => selectedCurses.includes(tag) && tag !== '');
-    if ((selectedSubraces.length === 0 || foundSubrace) &&
-        (selectedSubclasses.length === 0 || foundSubclass) &&
-        (selectedCurses.length === 0 || foundCurse)) {
-      results.push(detailsTags[i].outerHTML);
-    }
-  }
+//   const detailsTags = document.getElementsByTagName("details");
+//   const results = [];
+//   for (let i = 0; i < detailsTags.length; i++) {
+//     const tags = detailsTags[i].getAttribute("data-tags").split(",");
+//     const text = detailsTags[i].textContent.toLowerCase();
+//     const foundSubrace = tags.some(tag => selectedSubraces.includes(tag) && tag !== '');
+//     const foundSubclass = tags.some(tag => selectedSubclasses.includes(tag) && tag !== '');
+//     const foundCurse = tags.some(tag => selectedCurses.includes(tag) && tag !== '');
+//     if ((selectedSubraces.length === 0 || foundSubrace) &&
+//         (selectedSubclasses.length === 0 || foundSubclass) &&
+//         (selectedCurses.length === 0 || foundCurse)) {
+//       results.push(detailsTags[i].outerHTML);
+//     }
+//   }
 
-  const uniqueResults = [...new Set(results)];
-  const searchResults = document.getElementById("searchResults");
-  searchResults.innerHTML = "";
-  if (uniqueResults.length > 0) {
-    for (let i = 0; i < uniqueResults.length; i++) {
-      const result = document.createElement("li");
-      result.innerHTML = uniqueResults[i];
-      searchResults.appendChild(result);
-    }
-  } else {
-    const noResult = document.createElement("text");
-    noResult.innerHTML = "Aucun résultat trouvé.";
-    searchResults.appendChild(noResult);
-  }
-}
+//   const uniqueResults = [...new Set(results)];
+//   const searchResults = document.getElementById("searchResults");
+//   searchResults.innerHTML = "";
+//   if (uniqueResults.length > 0) {
+//     for (let i = 0; i < uniqueResults.length; i++) {
+//       const result = document.createElement("li");
+//       result.innerHTML = uniqueResults[i];
+//       searchResults.appendChild(result);
+//     }
+//   } else {
+//     const noResult = document.createElement("text");
+//     noResult.innerHTML = "Aucun résultat trouvé.";
+//     searchResults.appendChild(noResult);
+//   }
+// }
